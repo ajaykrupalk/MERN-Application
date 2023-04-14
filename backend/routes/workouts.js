055fc04cd51fda12b8a8b571f269bd70b9fa725e
ajaykrupalk <ajaykrupalk@outlook.in>
2023-04-14 20:38:04 +0530

    Initial Project

---
+++ backend/routes/workouts.js
@@ -0,0 +1,28 @@
+const express = require('express')
+const {
+    getWorkouts,
+    getWorkout,
+    createWorkout,
+    deleteWorkout,
+    updateWorkout
+} = require("../controllers/workoutController")
+
+
+const router = express.Router()
+
+//GET all workouts
+router.get('/', getWorkouts)
+
+//GET a single workout
+router.get('/:id', getWorkout)
+
+//POST a single workout
+router.post('/', createWorkout)
+
+//DELETE a single workout
+router.delete('/:id', deleteWorkout)
+
+//UPDATE a single workout
+router.patch('/:id', updateWorkout)
+
+module.exports = router
