edce689d90d1e766efaedd9dd6f53be4b773bf24
ajaykrupalk <ajaykrupalk@outlook.in>
2023-04-18 20:21:50 +0530

    Frontend

--- frontend/src/pages/Home.js
+++ frontend/src/pages/Home.js
@@ -1,17 +1,19 @@
-import { useEffect } from "react"
+import { useEffect, useState } from "react"
 
 // components
 import WorkoutDetails from "../components/WorkoutDetails"
 import WorkoutForm from "../components/WorkoutForm"
 
 const Home = () => {
+  const [workouts, setWorkouts] = useState(null)
+
   useEffect(() => {
     const fetchWorkouts = async () => {
       const response = await fetch('/api/workouts')
       const json = await response.json()
 
       if (response.ok) {
-        console.log(json);
+        setWorkouts(json)
       }
     }
 
